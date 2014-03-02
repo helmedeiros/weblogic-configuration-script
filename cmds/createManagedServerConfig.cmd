e8e821c3ea4fd8c8a1cbe9aa1b2f7a8a5b7d0d6f
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-03-01 22:40:25 -0300

    Fixed setWLSEnv call

--- cmds/createManagedServerConfig.cmd
+++ cmds/createManagedServerConfig.cmd
@@ -2,7 +2,7 @@
 @rem This script is used to set up the Managed Servers properties to DFSL   
 @rem *************************************************************************
 @echo At the first time configure the Classapath 
-CALL "%WORKSPACE%\wlst-scripts\setWLSEnvironment.cmd"
+CALL "%WORKSPACE%\cmds\setWLSEnvironment.cmd"
 @echo off
 @echo After the classpath configuration call the ANT task.
 @echo Execute the Ant Task
