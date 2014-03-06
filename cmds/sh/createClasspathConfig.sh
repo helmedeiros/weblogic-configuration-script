a7c4cd95a0e024799b79aac42e3ec6704c31f4e4
Helio Medeiros <helio.cabralmedeiros@gmail.com>
2014-03-05 22:24:12 -0300

    fix the ant task t obe called into vmargs and classpath definition

--- cmds/sh/createClasspathConfig.sh
+++ cmds/sh/createClasspathConfig.sh
@@ -10,7 +10,7 @@ echo "#*********FINISHED ENVIRONMENT CONFIGURATION**********#";
 
 echo "#*********STARTING MANAGED SERVER CLASSPATH CONFIGURATION**********#";
 echo "Execute the ant task";
-ant createVMArgsConfig -buildfile "$WORKSPACE/build.xml" -Dbuild.env=server -DconfigEnv=$1 -DconfigAction=$2
+ant createClasspathConfig -buildfile "$WORKSPACE/build.xml" -Dbuild.env=server -DconfigEnv=$1 -DconfigAction=$2
 echo "Finished the ant task execution";
 echo "#*********FINISHED MANAGED SERVER CLASSPATH CONFIGURATION**********#";
 
--- cmds/sh/createVMArgsConfig.sh
+++ cmds/sh/createVMArgsConfig.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #*****************************************************************************
-# This script is used to set up the Managed Servers BASED ON THE CONF FILES   
+# This script is used to set up the VMARGS properties BASED ON THE CONF FILES   
 #*****************************************************************************
 WORKSPACE=$(cd ../../$(dirname $0) && pwd);
 
@@ -8,10 +8,10 @@ echo "#*********STARTING ENVIRONMENT CONFIGURATION**********#";
 source $WORKSPACE/cmds/sh/setWLSEnv.sh;
 echo "#*********FINISHED ENVIRONMENT CONFIGURATION**********#";
 
-echo "#*********STARTING MANAGED SERVER CONFIGURATION**********#";
+echo "#*********STARTING MANAGED SERVER VM ARGS CONFIGURATION**********#";
 echo "Execute the ant task";
-ant createManagedServerConfig -buildfile "$WORKSPACE/build.xml" -Dbuild.env=server -DconfigEnv=$1 -DconfigAction=$2
+ant createVMArgsConfig -buildfile "$WORKSPACE/build.xml" -Dbuild.env=server -DconfigEnv=$1 -DconfigAction=$2
 echo "Finished the ant task execution";
-echo "#*********FINISHED MANAGED SERVER CONFIGURATION**********#";
+echo "#*********FINISHED MANAGED SERVER  VM ARGS CONFIGURATION**********#";
 
 exit 1;
